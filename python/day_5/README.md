b4498834b2d03b71f64b5b825553fdb08d9b1418
Sukh Sodhi <sukh00422@gmail.com>
2024-05-18 20:05:21 -0700

    second problem

--- python/day_5/README.md
+++ python/day_5/README.md
@@ -1,3 +1,7 @@
 # 5 kyu: https://www.codewars.com/kata/count-ip-addresses/train/javascript
 
 # 5 kyu: https://www.codewars.com/kata/longest-common-subsequence/train/javascript
+
+# 6 kyu: https://www.codewars.com/kata/validate-credit-card-number/train/javascript
+
+
---
+++ python/day_5/valid_credit_card.py
@@ -0,0 +1,32 @@
+def validate(n):
+    length = len(str(n))
+
+    number = str(n)
+    sum = int(number[-1])
+    double = True
+
+    for i in range(len(number) - 2, -1, -1):
+
+        digit = int(number[i])
+
+        if double:
+            digit *= 2
+            double = False
+        else:
+            double = True
+
+        if digit > 9:
+            digit -= 9
+        
+        sum += digit
+
+
+    print(sum % 10 == 0)
+
+
+
+
+
+
+
+validate(1234)
