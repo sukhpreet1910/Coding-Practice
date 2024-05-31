4ebc24777e50d809e721ba2a916149cf6b72050c
Sukh Sodhi <sukh00422@gmail.com>
2024-05-30 23:50:28 -0700

    better solution

--- python/day_7/set_matrix_zeros.py
+++ python/day_7/set_matrix_zeros.py
@@ -1,3 +1,5 @@
+# Brute Force (n^3)
+
 def set_zeroes(arr):
     flag = True
     index = []
@@ -16,4 +18,35 @@ def set_zeroes(arr):
     print(arr)
 
 arr = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
-set_zeroes(arr)
+# set_zeroes(arr)
+
+
+
+# Better Solution O(n^2)
+
+
+
+def set_zeros1(arr):
+    rows_len = len(arr)
+    columns_len = len(arr[0])
+
+    row = [0] * rows_len
+    column = [0] * columns_len
+
+    for i in range(rows_len):
+          for j in range(columns_len):
+               if arr[i][j] == 0:
+                    row[i] = 1
+                    column[j] = 1
+                
+    for i in range(rows_len):
+        for j in range(columns_len):
+             if row[i] or column[j]:
+                  arr[i][j] = 0
+    print('better', arr)
+
+
+arr = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+set_zeros1(arr)
+
+
